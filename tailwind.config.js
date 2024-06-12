a16ae941d6e7f18447c496bee32bf5e95eb8788d
yamamoto99 <yamamoto1952hkb@gmail.com>
2024-06-12 18:13:47 +0900

    fix:config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,js}'],
+  content: ['./src/js/*.js', './src/*.html'],
   important: true,
   darkMode: ['selector', '.dark'],
 }
